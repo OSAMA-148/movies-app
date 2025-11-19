9c1a9d9182fabeddc6783649e75ff55efc74e9c3
osama <om2951855@gmail.com>
2025-11-19 03:07:27 +0200

    refactor: clean up MovieCard component and remove unnecessary React import from Spinner

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,35 +1,55 @@
-import React from 'react'
+import PropTypes from 'prop-types';
 
-const MovieCard = ({ movie:
-  { title, vote_average, poster_path, release_date, original_language }
+const MovieCard = ({
+    movie: {
+        title,
+        vote_average,
+        poster_path,
+        release_date,
+        original_language,
+    },
 }) => {
-  return (
-    <div className="movie-card">
-      <img
-        src={poster_path ?
-          `https://image.tmdb.org/t/p/w500/${poster_path}` : '/no-movie.png'}
-        alt={title}
-      />
+    return (
+        <div className="movie-card">
+            <img
+                src={
+                    poster_path
+                        ? `https://image.tmdb.org/t/p/w500/${poster_path}`
+                        : "/no-movie.png"
+                }
+                alt={title}
+            />
 
-      <div className="mt-4">
-        <h3>{title}</h3>
+            <div className="mt-4">
+                <h3>{title}</h3>
 
-        <div className="content">
-          <div className="rating">
-            <img src="star.svg" alt="Star Icon" />
-            <p>{vote_average ? vote_average.toFixed(1) : 'N/A'}</p>
-          </div>
+                <div className="content">
+                    <div className="rating">
+                        <img src="star.svg" alt="Star Icon" />
+                        <p>{vote_average ? vote_average.toFixed(1) : "N/A"}</p>
+                    </div>
 
-          <span>•</span>
-          <p className="lang">{original_language}</p>
+                    <span>•</span>
+                    <p className="lang">{original_language}</p>
 
-          <span>•</span>
-          <p className="year">
-            {release_date ? release_date.split('-')[0] : 'N/A'}
-          </p>
+                    <span>•</span>
+                    <p className="year">
+                        {release_date ? release_date.split("-")[0] : "N/A"}
+                    </p>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  )
-}
-export default MovieCard
+    );
+};
+
+MovieCard.propTypes = {
+    movie: PropTypes.shape({
+        title: PropTypes.string,
+        vote_average: PropTypes.number,
+        poster_path: PropTypes.string,
+        release_date: PropTypes.string,
+        original_language: PropTypes.string,
+    }).isRequired,
+};
+
+export default MovieCard;
